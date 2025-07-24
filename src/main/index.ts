1f52135f40c7971c1d0d2c85598d6293478e4d04
Violet2314 <3279499388@qq.com>
2025-07-24 15:31:06 +0800

    package

---
+++ src/main/index.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  })
+
+  // 开发环境加载开发服务器，生产环境加载构建文件
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173')
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
---
+++ src/preload/index.ts
@@ -0,0 +1,2 @@
+// 最简单的预加载脚本 - 暂时为空
+console.log('Preload script loaded')
